ecb60352d9f7a88921427f39ef96203f7ef0ddff
bevanvarghese <bevanvar@connect.hku.hk>
2020-08-11 20:26:47 +0530

    React setup

---
+++ krowten-client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+//components
+import Navbar from './components/Navbar';
+
+//pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={home}></Route>
+            <Route exact path='/login' component={login}></Route>
+            <Route exact path='/signup' component={signup}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
