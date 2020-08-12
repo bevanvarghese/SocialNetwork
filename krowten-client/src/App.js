25f059e68dc6a0432af022cf5ff62a981a384445
bevanvarghese <bevanvar@connect.hku.hk>
2020-08-13 00:09:45 +0530

    Login page done - CORS needed

--- krowten-client/src/App.js
+++ krowten-client/src/App.js
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 //components
 import Navbar from './components/Navbar';
-
 //pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={home}></Route>
-            <Route exact path='/login' component={login}></Route>
-            <Route exact path='/signup' component={signup}></Route>
-          </Switch>
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#011e28',
+      main: '#022b3a',
+      dark: '#345561',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#155562',
+      main: '#1F7A8C',
+      dark: '#4b94a3',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className='App'>
+          <Router>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={home}></Route>
+                <Route exact path='/login' component={login}></Route>
+                <Route exact path='/signup' component={signup}></Route>
+              </Switch>
+            </div>
+          </Router>
         </div>
-      </Router>
-    </div>
-  );
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
--- krowten-client/src/pages/login.js
+++ krowten-client/src/pages/login.js
@@ -1,13 +1,173 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-export class login extends Component {
+//material ui stuff
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+axios.defaults.baseURL =
+  'https://asia-east2-krowten-ffb6f.cloudfunctions.net/api.com';
+
+const styles = {
+  form: {
+    textAlign: 'center',
+  },
+  image: {
+    margin: '20px auto 20px auto',
+    maxWidth: '120px',
+    maxHeight: '120px',
+  },
+  //card: {
+  //backgroundColor: '#ffffff',
+  //},
+  pageTitle: {
+    margin: 'auto auto auto auto',
+  },
+  textField: {
+    margin: '10px auto 10px auto',
+  },
+  button: {
+    margin: '10px auto auto auto',
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    margin: '10px',
+  },
+  progress: {
+    position: 'absolute',
+  },
+};
+
+class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    console.log(userData);
+    axios
+      .post('/login', userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false,
+        });
+        //method to push state to URL
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        this.setState({
+          error: err.response,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    //we get a "target" which is the name of the field changed
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    //vvv using three sm Grids does 12/3 = 4 colsize automatically vvv
     return (
-      <div>
-        <h1>Login page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt='logo' className={classes.image} />
+          <Typography
+            variant='h3'
+            color='primary'
+            className={classes.pageTitle}
+          >
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id='email'
+              name='email'
+              type='email'
+              label='Email'
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id='password'
+              name='password'
+              type='password'
+              label='Password'
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant='body2' className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              className={classes.button}
+              disabled={loading}
+            >
+              Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Don't have an account? Sign up{' '}
+              <Link to='/signup' color='primary'>
+                here
+              </Link>
+              .
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
